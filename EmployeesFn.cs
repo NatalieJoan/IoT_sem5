bb89f4a09829aac81536d4a45d9e3f318b9f6d13
NatalieJoan <nhelak2001@gmail.com>
2024-01-15 13:09:01 +0100

    Rozszerzono requesty w EmployeesFn.cs

--- EmployeesFn.cs
+++ EmployeesFn.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using System.Text.Json;
+using Azure;
 using Employees.Service;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -21,22 +23,35 @@ namespace Company.Function
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
             switch (req.Method)
             {
                 case "POST":
+                    StreamReader reader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
+                    var json = reader.ReadToEnd();
+                    var employee = JsonSerializer.Deserialize<Employee>(json);
+                    var res = employeesService.Add(employee._firstName, employee._lastName);
+                    response.WriteAsJsonAsync(res);
                     break;
                 case "PUT":
+                    StreamReader putReader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
+                    var putJson = putReader.ReadToEnd();
+                    var putEmployee = JsonSerializer.Deserialize<Employee>(putJson);
+                    var putRes = employeesService.Update(putEmployee.Id, putEmployee._firstName, putEmployee._lastName);
+                    response.WriteAsJsonAsync(putRes);
                     break;
                 case "GET":
+                    var employees = employeesService.Get();
+                    response.WriteAsJsonAsync(employees);
                     break;
                 case "DELETE":
+                    StreamReader deleteReader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
+                    var deleteJson = deleteReader.ReadToEnd();
+                    var hireEmployee = JsonSerializer.Deserialize<Employee>(deleteJson);
+                    employeesService.Delete(hireEmployee.Id);
                     break;
             }
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
 
             return response;
         }
